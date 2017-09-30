5daf3bb6d7063840e9d75a9342d380f2e34db8f4
文优 <wenyou.ly@taobao.com>
2017-09-30 17:06:51 +0800

    update

---
+++ SwiftMap/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftMap
+//
+//  Created by wenyou on 2017/9/30.
+//  Copyright © 2017年 wenyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
